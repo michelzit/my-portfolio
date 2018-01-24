77438396b1509808dd7e25603166ec9c578f6ae9
Michel Zitman <michelzit@gmail.com>
2018-01-24 14:25:08 +0100

    update portfolio deployment function for lambda

--- upload-portfolio-lambda.py
+++ upload-portfolio-lambda.py
@@ -4,17 +4,20 @@ import io
 import zipfile
 import mimetypes
 
-s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+def lambda_handler(event, context):
+    s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
 
-serverless_bucket = s3.Bucket('zitman.cloud')
-build_bucket = s3.Bucket('zitmancloudserverless')
+    serverless_bucket = s3.Bucket('zitman.cloud')
+    build_bucket = s3.Bucket('zitmancloudserverless')
 
-portfolio_zip = io.BytesIO()
-build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+    portfolio_zip = io.BytesIO()
+    build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
 
+    with zipfile.ZipFile(portfolio_zip) as myzip:
+        for nm in myzip.namelist():
+            obj = myzip.open(nm)
+            serverless_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+            serverless_bucket.Object(nm).Acl().put(ACL='public-read')
 
-with zipfile.ZipFile(portfolio_zip) as myzip:
-    for nm in myzip.namelist():
-        obj = myzip.open(nm)
-        serverless_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
-        serverless_bucket.Object(nm).Acl().put(ACL='public-read')
+    print ("Job done!")
+    return 'Hello from Lambda'
